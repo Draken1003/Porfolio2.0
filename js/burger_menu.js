917b5b0293f2629941c7f7e7575a46f16ccbe146
Draken <hetitan10@gmail.com>
2025-05-18 13:47:52 +0200

    burger-menu

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -1,28 +1,33 @@
-let open = false;
-
 function openBurger(burgerElement) {
-    burgerElement.classList.toggle("active");
     const menu = document.getElementById("bm");
 
-
-    if (!open) {
-        menu.classList.remove("burger-menu-open");
-        menu.classList.add("burger-menu-close");
-    } else {
-        menu.classList.remove("burger-menu-close");
-        menu.classList.add("burger-menu-open");
+    if (menu.classList.contains("close")){
+        menu.classList.remove("close");
+        menu.classList.add("open");
+        burgerElement.classList.add("active");
+        menu.style.display = "block";
     }
-
-    open = !open;
+    else{
+        menu.classList.remove("open");
+        menu.classList.add("close");
+        burgerElement.classList.remove("active");
+        setTimeout(() => {
+            menu.style.display = "none";
+        }, 500);
+        
+    }
+    
 }
 
 function closeBurger() {
     const burger = document.querySelector(".burger");
     const menu = document.getElementById("bm");
+    const body = document.body;
 
     burger.classList.remove("active");
     menu.classList.remove("burger-menu-open");
     menu.classList.add("burger-menu-close");
+    body.style.overflowY = "scroll"; // réactive le scroll
 
     open = false;
 }

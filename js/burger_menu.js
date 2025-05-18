333d7d1639e90b272cfd313f93210d527af0919d
Draken <hetitan10@gmail.com>
2025-05-18 20:32:13 +0200

    finission burger menu

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -2,18 +2,23 @@ function openBurger(burgerElement) {
     const menu = document.getElementById("bm");
 
     if (menu.classList.contains("close")){
+        document.documentElement.style.overflow = "hidden";
+        document.body.style.overflow = "hidden";
         menu.classList.remove("close");
         menu.classList.add("open");
         burgerElement.classList.add("active");
         menu.style.display = "block";
+        
     }
     else{
+        document.documentElement.style.overflow = "auto";
+        document.body.style.overflow = "auto";
         menu.classList.remove("open");
         menu.classList.add("close");
         burgerElement.classList.remove("active");
         setTimeout(() => {
             menu.style.display = "none";
-        }, 500);
+        }, 500);        
         
     }
     
